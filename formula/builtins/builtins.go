4a709bd27f193ed81ebc15709562de02529f4c53
Nicolas Brun <nbrun@gld.be>
2026-02-13 21:24:13 +0100

    extends default set of builtins available

--- formula/builtins/builtins.go
+++ formula/builtins/builtins.go
@@ -9,10 +9,36 @@ import (
 var ErrArity = errors.New("invalid number of arguments")
 
 var Registry = map[string]func([]value.Value) (value.Value, error){
-	"min":    Min,
-	"max":    Max,
-	"sum":    Sum,
-	"avg":    Avg,
-	"count":  Count,
-	"typeof": TypeOf,
+	"min":       Min,
+	"max":       Max,
+	"sum":       Sum,
+	"avg":       Avg,
+	"average":   Avg,
+	"count":     Count,
+	"round":     Round,
+	"rounddown": Floor,
+	"roundup":   Ceil,
+	"sqrt":      Sqrt,
+	"typeof":    TypeOf,
+	"now":       Now,
+	"rand":      Rand,
+	"isnumber":  IsNumber,
+	"istext":    IsText,
+	"concat":    Concat,
+	"left":      Left,
+	"right":     Right,
+	"mid":       Mid,
+	"len":       Len,
+	"upper":     Upper,
+	"lower":     Lower,
+	"substr":    Substr,
+	"replace":   Replace,
+}
+
+func Now(args []value.Value) (value.Value, error) {
+	return nil, nil
+}
+
+func Rand(args []value.Value) (value.Value, error) {
+	return nil, nil
 }
