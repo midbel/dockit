3edf49809d4cc73e80e54bfb97e01725eb73ab5e
Nicolas Brun <nbrun@gld.be>
2026-03-09 21:36:56 +0100

    basic read content.xml

--- ods/ods.go
+++ ods/ods.go
@@ -1,6 +1,15 @@
 package ods
 
 import (
+	"fmt"
+	"io"
+	"iter"
+	"maps"
+	"math"
+	"os"
+	"slices"
+
+	"github.com/midbel/dockit/grid"
 	"github.com/midbel/dockit/layout"
 	"github.com/midbel/dockit/value"
 )
@@ -33,9 +42,7 @@ func (c *Cell) Reload(ctx value.Context) error {
 }
 
 type row struct {
-	Line   int64
-	Hidden bool
-	Cells  []*Cell
+	Cells []*Cell
 }
 
 func (r *row) Values() []value.ScalarValue {
@@ -80,6 +87,111 @@ func (s *Sheet) Name() string {
 	return s.Label
 }
 
+func (s *Sheet) View(rg *layout.Range) grid.View {
+	bd := s.Bounds()
+	rg.Starts = rg.Starts.Update(bd.Starts)
+	rg.Ends = rg.Ends.Update(bd.Ends)
+	return grid.NewBoundedView(s, rg)
+}
+
+func (s *Sheet) Sub(start, end layout.Position) grid.View {
+	return s.View(layout.NewRange(start, end))
+}
+
+func (s *Sheet) Cell(pos layout.Position) (grid.Cell, error) {
+	cell, _ := s.cells[pos]
+	return cell, nil
+}
+
+func (s *Sheet) Reload(ctx value.Context) error {
+	return nil
+}
+
+func (s *Sheet) Bounds() *layout.Range {
+	var (
+		minRow int64 = math.MaxInt64
+		maxRow int64
+		minCol int64 = math.MaxInt64
+		maxCol int64
+	)
+	for c := range maps.Values(s.cells) {
+		minRow = min(minRow, c.Line)
+		maxRow = max(maxRow, c.Line)
+		minCol = min(minCol, c.Column)
+		maxCol = max(maxCol, c.Column)
+	}
+	var rg layout.Range
+	if maxRow == 0 || maxCol == 0 {
+		pos := layout.Position{
+			Line:   1,
+			Column: 1,
+		}
+		rg.Starts = pos
+		rg.Ends = pos
+	} else {
+		rg.Starts = layout.Position{
+			Line:   minRow,
+			Column: minCol,
+		}
+		rg.Ends = layout.Position{
+			Line:   maxRow,
+			Column: maxCol,
+		}
+	}
+	return &rg
+}
+
+func (s *Sheet) Rows() iter.Seq[[]value.ScalarValue] {
+	it := func(yield func([]value.ScalarValue) bool) {
+		for _, r := range s.rows {
+			row := r.Values()
+			if len(r.Cells) == 0 {
+				continue
+			}
+			if !yield(row) {
+				break
+			}
+		}
+	}
+	return it
+}
+
+func (s *Sheet) SetValue(pos layout.Position, val value.ScalarValue) error {
+	return nil
+}
+
+func (s *Sheet) SetFormula(pos layout.Position, expr value.Formula) error {
+	return nil
+}
+
+func (s *Sheet) ClearCell(pos layout.Position) error {
+	return nil
+}
+
+func (s *Sheet) ClearValue(pos layout.Position) error {
+	return nil
+}
+
+func (s *Sheet) ClearRange(rg *layout.Range) error {
+	return nil
+}
+
+func (s *Sheet) ClearFormula(pos layout.Position) error {
+	return nil
+}
+
+func (s *Sheet) AppendRow(values []value.ScalarValue) error {
+	return nil
+}
+
+func (s *Sheet) InsertRow(ix int64, values []value.ScalarValue) error {
+	return nil
+}
+
+func (s *Sheet) DeleteRow(ix int64) error {
+	return nil
+}
+
 type File struct {
 	names  map[string]int
 	sheets []*Sheet
@@ -102,4 +214,103 @@ func Open(file string) (*File, error) {
 		return nil, err
 	}
 	return book, nil
+}
+
+func (f *File) WriteFile(file string) error {
+	w, err := os.Create(file)
+	if err == nil {
+		defer w.Close()
+		err = f.WriteTo(w)
+	}
+	return err
+}
+
+func (f *File) WriteTo(w io.Writer) error {
+	return nil
+}
+
+func (f *File) Infos() []grid.ViewInfo {
+	var infos []grid.ViewInfo
+	for _, s := range f.sheets {
+		i := grid.ViewInfo{
+			Name:      s.Name(),
+			Active:    false,
+			Protected: false,
+			Hidden:    false,
+			Size:      s.Size,
+		}
+
+		infos = append(infos, i)
+	}
+	return infos
+}
+
+func (f *File) Reload() error {
+	return nil
+}
+
+func (f *File) ActiveSheet() (grid.View, error) {
+	return f.activeSheet()
+}
+
+func (f *File) Sheet(name string) (grid.View, error) {
+	return f.sheetByName(name)
+}
+
+func (f *File) Sheets() []grid.View {
+	n := len(f.sheets)
+	if n == 0 {
+		return nil
+	}
+	views := make([]grid.View, 0, n)
+	for i := range f.sheets {
+		views = append(views, f.sheets[i])
+	}
+	return views
+}
+
+// rename a sheet
+func (f *File) Rename(oldName, newName string) error {
+	return nil
+}
+
+// copy a sheet
+func (f *File) Copy(oldName, newName string) error {
+	return nil
+}
+
+func (f *File) RemoveSheet(name string) error {
+	return nil
+}
+
+func (f *File) AppendSheet(sheet grid.View) error {
+	return nil
+}
+
+// append sheets of given file to current fule
+func (f *File) Merge(other grid.File) error {
+	return nil
+}
+
+func (f *File) activeSheet() (*Sheet, error) {
+	if len(f.sheets) == 1 {
+		return f.sheets[0], nil
+	}
+	ix := slices.IndexFunc(f.sheets, func(s *Sheet) bool {
+		return s.Active == true
+	})
+	if ix < 0 {
+		return nil, fmt.Errorf("missing active sheet")
+	}
+	return f.sheets[ix], nil
+}
+
+func (f *File) sheetByName(name string) (*Sheet, error) {
+	ix := slices.IndexFunc(f.sheets, func(s *Sheet) bool {
+		return s.Name() == name
+	})
+	if ix < 0 {
+		return nil, fmt.Errorf("sheet %s %w", name, grid.ErrFound)
+	}
+	return f.sheets[ix], nil
 }
--- ods/reader.go
+++ ods/reader.go
@@ -5,12 +5,20 @@ import (
 	"fmt"
 	"io"
 	"slices"
+	"strconv"
 
 	sax "github.com/midbel/codecs/xml"
 	"github.com/midbel/dockit/grid"
+	"github.com/midbel/dockit/layout"
+	"github.com/midbel/dockit/value"
 )
 
 const mimeODS = "application/vnd.oasis.opendocument.spreadsheet"
+
+const (
+	tableNS = "urn:oasis:names:tc:opendocument:xmlns:table:1.0"
+	textNS  = "urn:oasis:names:tc:opendocument:xmlns:text:1.0"
+)
 
 type reader struct {
 	reader *zip.ReadCloser
@@ -61,8 +69,6 @@ func (r *reader) readMime(file *File) {
 	}
 }
 
-const tableNS = "urn:oasis:names:tc:opendocument:xmlns:table:1.0"
-
 func (r *reader) readContent(file *File) {
 	if r.invalid() {
 		return
@@ -80,7 +86,7 @@ func (r *reader) readContent(file *File) {
 		sr := updateSheet(e.GetAttributeValue("name"), rs)
 		sh, err := sr.Update()
 		if err == nil {
-			_ = sh
+			file.sheets = append(file.sheets, sh)
 		}
 		return err
 	})
@@ -102,17 +108,82 @@ func (r *reader) invalid() bool {
 }
 
 type sheetReader struct {
-	sh *Sheet
+	sh     *Sheet
 	reader *sax.Reader
 }
 
 func updateSheet(name string, rs *sax.Reader) *sheetReader {
 	return &sheetReader{
-		sh: NewSheet(name),
+		sh:     NewSheet(name),
 		reader: rs,
 	}
 }
 
 func (r *sheetReader) Update() (*Sheet, error) {
+	var (
+		qCell = sax.ExpandedName("table-cell", "table", tableNS)
+		qRow  = sax.ExpandedName("table-row", "table", tableNS)
+	)
+	r.reader.Element(qRow, r.onRow)
+	r.reader.Element(qCell, r.onCell)
 	return r.sh, nil
+}
+
+func (r *sheetReader) onRow(_ *sax.Reader, e sax.E) error {
+	var rs row
+	r.sh.rows = append(r.sh.rows, &rs)
+	r.sh.Size.Lines++
+	return nil
+}
+
+func (r *sheetReader) onCell(rs *sax.Reader, e sax.E) error {
+	if len(r.sh.rows) == 0 {
+		return fmt.Errorf("no row in worksheet")
+	}
+	var (
+		pos  = len(r.sh.rows) - 1
+		cell = &Cell{
+			Position: layout.Position{
+				Line:   int64(pos + 1),
+				Column: int64(len(r.sh.rows[pos].Cells)) + 1,
+			},
+		}
+	)
+	r.sh.rows[pos].Cells = append(r.sh.rows[pos].Cells, cell)
+	r.sh.cells[cell.Position] = cell
+
+	switch e.GetAttributeValue("value-type") {
+	case "float", "currency", "percentage":
+		cell.raw = e.GetAttributeValue("value")
+		val, err := strconv.ParseFloat(cell.raw, 64)
+		if err != nil {
+			return err
+		}
+		cell.parsed = value.Float(val)
+	case "boolean":
+		cell.raw = e.GetAttributeValue("boolean-value")
+		val, err := strconv.ParseBool(cell.raw)
+		if err != nil {
+			return err
+		}
+		cell.parsed = value.Boolean(val)
+	case "date":
+		cell.raw = e.GetAttributeValue("date-value")
+	case "time":
+		cell.raw = e.GetAttributeValue("time-value")
+	default:
+	}
+	if cell.raw != "" {
+		return nil
+	}
+	qn := sax.ExpandedName("p", "text", textNS)
+	rs.Element(qn, func(rs *sax.Reader, _ sax.E) error {
+		rs.OnText(func(_ *sax.Reader, str string) error {
+			cell.raw = str
+			cell.parsed = value.Text(str)
+			return nil
+		})
+		return nil
+	})
+	return nil
 }
