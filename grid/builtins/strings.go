37baa11624e2badb609d7727200cb1a4d7441344
Nicolas Brun <nbrun@gld.be>
2026-03-23 20:39:02 +0100

    additional builtins

--- grid/builtins/strings.go
+++ grid/builtins/strings.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 	"unicode"
 
+	"github.com/midbel/dockit/grid/format"
 	"github.com/midbel/dockit/value"
 )
 
@@ -203,23 +204,98 @@ func Find(args []value.Value) value.Value {
 }
 
 func Replace(args []value.Value) value.Value {
-	return nil
+	if err := value.HasErrors(args...); err != nil {
+		return err
+	}
+	var (
+		str  = asString(args[0])
+		pos  = asFloat(args[1])
+		num  = asFloat(args[2])
+		repl = asString(args[3])
+	)
+	if int(pos) >= len(str) {
+		return value.Text(str)
+	}
+	if int(pos+num) >= len(str) {
+		return value.Text(str[:int(pos)+1] + repl)
+	}
+	str = str[:int(pos)+1] + repl + str[int(pos+num):]
+	return value.Text(str)
 }
 
 func Substitute(args []value.Value) value.Value {
-	return nil
+	if err := value.HasErrors(args...); err != nil {
+		return err
+	}
+	var (
+		str  = asString(args[0])
+		old  = asString(args[1])
+		repl = asString(args[2])
+		num  = asFloat(args[3])
+	)
+	if num <= 0 {
+		return value.ErrValue
+	}
+	str = strings.Replace(str, old, repl, int(num))
+	return value.Text(str)
 }
 
 func Text(args []value.Value) value.Value {
-	return nil
+	if err := value.HasErrors(args...); err != nil {
+		return err
+	}
+	var (
+		str = asString(args[1])
+		ft  format.Formatter
+		err error
+	)
+	switch args[0].Type() {
+	case value.TypeNumber:
+		ft, err = format.ParseNumberFormatter(str)
+	case value.TypeDate:
+		ft, err = format.ParseDateFormatter(str)
+	default:
+		return value.ErrValue
+	}
+	if err != nil {
+		return value.ErrValue
+	}
+	str, err = ft.Format(args[0])
+	if err != nil {
+		return value.ErrValue
+	}
+	return value.Text(str)
 }
 
 func Value(args []value.Value) value.Value {
-	return nil
+	if err := value.HasErrors(args...); err != nil {
+		return err
+	}
+	v, err := value.CastToFloat(args[0])
+	if err != nil {
+		return value.ErrValue
+	}
+	return v
 }
 
 func Textjoin(args []value.Value) value.Value {
-	return nil
+	if err := value.HasErrors(args...); err != nil {
+		return err
+	}
+	var (
+		delim  = asString(args[0])
+		ignore = asBool(args[1])
+		parts  = make([]string, 0, len(args))
+	)
+	for i := 2; i < len(args); i++ {
+		str := asString(args[i])
+		if str == "" && ignore {
+			continue
+		}
+		parts = append(parts, str)
+	}
+	str := strings.Join(parts, delim)
+	return value.Text(str)
 }
 
 func Exact(args []value.Value) value.Value {
