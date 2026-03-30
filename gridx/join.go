d5a0ecbdea625241d393f46408f8f7e839fe605a
Nicolas Brun <nbrun@gld.be>
2026-03-30 20:52:35 +0200

    building join

--- gridx/join.go
+++ gridx/join.go
@@ -9,15 +9,30 @@ import (
 	"github.com/midbel/dockit/value"
 )
 
+type joinRow struct {
+	Left  int64
+	Right int64
+}
+
 type joinView struct {
 	left  grid.View
 	right grid.View
 
-	// index
+	rows []joinRow
 }
 
-func Join(left, right grid.View) grid.View {
-	return nil
+func Join(left, right grid.View, leftcols, rightcols layout.Selection) grid.View {
+	var (
+		index = createIndex(right, rightcols)
+		rows  = createLinks(left, leftcols, index)
+	)
+
+	j := joinView{
+		left:  left,
+		right: right,
+		rows:  rows,
+	}
+	return &j
 }
 
 func (v *joinView) Name() string {
@@ -41,6 +56,56 @@ func (v *joinView) Cell(layout.Position) (Cell, error) {
 
 func (v *joinView) Sync(value.Context) error {
 	return grid.ErrSupported
+}
+
+func keyFromRow(row []value.ScalarValue, cols []int64) string {
+	var ks []string
+	var b strings.Builder
+	for i, c := range cols {
+		if i > 0 {
+			b.WriteRune('|')
+		}
+		k := createKey(row[c])
+		b.WriteString(k)
+	}
+	return b.String()
+}
+
+func createLinks(view grid.View, keys layout.Selection, index map[string][]int64) []joinRow {
+	var (
+		lino int64
+		rows []joinRow
+		cols = keys.Indices(view.Bounds())
+	)
+
+	for rs := range view.Rows() {
+		lino++
+		k := keyFromRow(rs, cols)
+
+		matches := index[k]
+		for _, m := range matches {
+			r := joinRow{
+				Left:  lino,
+				Right: m,
+			}
+			rows = append(rows, r)
+		}
+	}
+	return rows
+}
+
+func createIndex(view grid.View, keys layout.Selection) map[string][]int64 {
+	var (
+		lino  int
+		cols  = keys.Indices(view.Bounds())
+		index = make(map[string][]int64)
+	)
+	for rs := range right.Rows() {
+		lino++
+		k := keyFromRow(rs, cols)
+		index[k] = append(index[k], lino)
+	}
+	return index
 }
 
 func createKey(v value.Value) string {
