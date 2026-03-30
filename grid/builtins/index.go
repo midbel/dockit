04c61799d87fe41517fd463f308878388c29c202
Nicolas Brun <nbrun@gld.be>
2026-03-30 19:34:46 +0200

    index builtin

--- grid/builtins/index.go
+++ grid/builtins/index.go
@@ -1,6 +1,8 @@
 package builtins
 
 import (
+	"math"
+	
 	"github.com/midbel/dockit/value"
 )
 
@@ -19,7 +21,7 @@ func Match(args []value.Value) value.Value {
 	if err := value.HasErrors(args...); err != nil {
 		return err
 	}
-	return value.FindIndex(args[1], args[0])
+	return value.FindIndex(args[1:], args[0])
 }
 
 var indexBuiltin = Builtin{
@@ -38,7 +40,18 @@ func Index(args []value.Value) value.Value {
 	if err := value.HasErrors(args...); err != nil {
 		return err
 	}
-	return nil
+	arr, ok := args[0].(value.ArrayValue)
+	if !ok {
+		return value.ErrValue
+	}
+	var (
+		row = asFloat(args[1])
+		col = 1.0
+	)
+	if len(args) >= 3 {
+		col = asFloat(args[2])
+	}
+	return arr.At(int(math.Floor(row)-1), int(math.Floor(col)-1))
 }
 
 var vlookupBuiltin = Builtin{
