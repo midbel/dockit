d4127e5dd3cf1fd8114cc136fe1a6bdb8ee49da0
Nicolas Brun <nbrun@gld.be>
2026-04-25 21:11:55 +0200

    refactor group test

--- gridx/group_test.go
+++ gridx/group_test.go
@@ -7,20 +7,89 @@ import (
 
 	"github.com/midbel/dockit/csv"
 	"github.com/midbel/dockit/flat"
+	"github.com/midbel/dockit/grid"
 	"github.com/midbel/dockit/internal/testutil"
 	"github.com/midbel/dockit/layout"
 )
 
-const groupSample = `go,foo,100,42
-go,foo,50,0
-go,bar,50,0
-ts,bar,1,0
-js,bar,5,1
-js,foo,15,58
-java,quz,10,12
-go,bar,1000,3`
+const groupSample = `go,foo,100
+go,foo,50
+go,bar,50
+ts,bar,1
+js,bar,5
+js,foo,15
+java,quz,10
+go,bar,1000`
+
+const numCol = 3
 
 func TestGroup(t *testing.T) {
+	t.Run("single-key", testGroupSingleKey)
+	t.Run("multi-key", testGroupMultiKey)
+}
+
+func testGroupMultiKey(t *testing.T) {
+	var (
+		want = [][]string{
+			{"ts", "bar", "1", "1", "1", "1", "1"},
+			{"js", "bar", "5", "5", "5", "5", "1"},
+			{"js", "foo", "15", "15", "15", "15", "1"},
+			{"java", "quz", "10", "10", "10", "10", "1"},
+			{"go", "bar", "1050", "50", "1000", "525", "2"},
+			{"go", "foo", "150", "50", "100", "75", "2"},
+		}
+		keys, _ = layout.SelectionFromString("A;B")
+		view    = createGroupView(t, keys)
+		got     = testutil.Collect(view)
+	)
+
+	compareGroupResult(t, want, got, func(rs1, rs2 []string) int {
+		res := strings.Compare(rs1[0], rs2[0])
+		if res == 0 {
+			res = strings.Compare(rs1[1], rs2[1])
+		}
+		return res
+	})
+}
+
+func testGroupSingleKey(t *testing.T) {
+	var (
+		want = [][]string{
+			{"java", "10", "10", "10", "10", "1"},
+			{"go", "1200", "50", "1000", "300", "4"},
+			{"ts", "1", "1", "1", "1", "1"},
+			{"js", "20", "5", "15", "10", "2"},
+		}
+		keys, _ = layout.SelectionFromString("A")
+		view    = createGroupView(t, keys)
+		got     = testutil.Collect(view)
+	)
+
+	compareGroupResult(t, want, got, func(rs1, rs2 []string) int {
+		return strings.Compare(rs1[0], rs2[0])
+	})
+}
+
+func compareGroupResult(t *testing.T, want, got [][]string, cmp func([]string, []string) int) {
+	t.Helper()
+
+	slices.SortFunc(got, cmp)
+	slices.SortFunc(want, cmp)
+
+	if len(got) != len(want) {
+		t.Fatalf("number of rows mismatched! want %d, got %d", len(want), len(got))
+		return
+	}
+	for i := 0; i < len(got); i++ {
+		if !slices.Equal(want[i], got[i]) {
+			t.Errorf("[%d] results mismatched! want %v, got %v", i, want[i], got[i])
+		}
+	}
+}
+
+func createGroupView(t *testing.T, keys layout.Selection) grid.View {
+	t.Helper()
+
 	r := csv.NewReader(strings.NewReader(groupSample))
 	f, err := flat.OpenReader(r)
 	if err != nil {
@@ -30,45 +99,17 @@ func TestGroup(t *testing.T) {
 	if err != nil {
 		t.Fatalf("fail to retrieve active sheet: %s", err)
 	}
-	var (
-		keys, _ = layout.SelectionFromString("A")
-		aggr    = []Aggr{
-			*NewAggr(3, Sum()),
-			*NewAggr(3, Min()),
-			*NewAggr(3, Max()),
-			*NewAggr(3, Avg()),
-			*NewAggr(3, Count()),
-		}
-	)
+
+	aggr := []Aggr{
+		*NewAggr(numCol, Sum()),
+		*NewAggr(numCol, Min()),
+		*NewAggr(numCol, Max()),
+		*NewAggr(numCol, Avg()),
+		*NewAggr(numCol, Count()),
+	}
 	view, err := Group(sh, keys, aggr)
 	if err != nil {
-		t.Fatalf("error creating group view")
+		t.Fatalf("error creating group view: %v", err)
 	}
-	want := [][]string{
-		{"java", "10", "10", "10", "10", "1"},
-		{"go", "1200", "50", "1000", "300", "4"},
-		{"ts", "1", "1", "1", "1", "1"},
-		{"js", "20", "5", "15", "10", "2"},
-	}
-	got := testutil.Collect(view)
-
-	slices.SortFunc(got, func(rs1, rs2 []string) int {
-		return strings.Compare(rs1[0], rs2[0])
-	})
-	slices.SortFunc(want, func(rs1, rs2 []string) int {
-		return strings.Compare(rs1[0], rs2[0])
-	})
-	if len(got) != len(want) {
-		t.Fatalf("number of rows mismatched! want %d, got %d", len(want), len(got))
-	}
-	for i := 0; i < len(got); i++ {
-		if len(got[i]) != len(want[i]) {
-			t.Errorf("number of values mismatched! want %d, got %d", len(want[i]), len(got[i]))
-		}
-		for j := 0; j < len(got[i]); j++ {
-			if got[i][j] != want[i][j] {
-				t.Errorf("values mismatched! want %s, got %s", want[i][j], got[i][j])
-			}
-		}
-	}
+	return view
 }
