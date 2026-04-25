02891f55ddc371eebe994a504fed9de46bab9578
Nicolas Brun <nbrun@gld.be>
2026-04-26 01:15:29 +0200

    testing join with multiple keys

--- gridx/join_test.go
+++ gridx/join_test.go
@@ -9,15 +9,35 @@ import (
 	"github.com/midbel/dockit/layout"
 )
 
-const leftJoinSample = `1,go,100
-2,js,50
-3,c,20`
-const rightJoinSample = `1,1,dockit,github.com/midbel/dockit
-2,1,sweet,github.com/midbel/sweet-ql
-3,2,dockit-ui,github.com/midbel/dockit-ui`
+const leftJoinSample = `1,go,100,midbel
+2,js,50,midbel
+3,c,20,midbel`
+const rightJoinSample = `1,1,midbel,dockit,github.com/midbel/dockit
+2,1,midbel,sweet,github.com/midbel/sweet-ql
+3,2,nobody,dockit-ui,github.com/midbel/dockit-ui`
 
 func TestJoin(t *testing.T) {
 	t.Run("single-key", testJoinSingleKey)
+	t.Run("multi-key", testJoinMultiKey)
+}
+
+func testJoinMultiKey(t *testing.T) {
+	var (
+		v1      = getJoinView(t, leftJoinSample)
+		v2      = getJoinView(t, rightJoinSample)
+		key1, _ = layout.SelectionFromString("A;D")
+		key2, _ = layout.SelectionFromString("B;C")
+	)
+
+	view := Join(v1, v2, key1, key2)
+
+	want := [][]string{
+		{"1", "go", "100", "midbel", "1", "1", "midbel", "dockit", "github.com/midbel/dockit"},
+		{"1", "go", "100", "midbel", "2", "1", "midbel", "sweet", "github.com/midbel/sweet-ql"},
+	}
+	got := testutil.Collect(view)
+	testutil.AssertSize(t, view, got)
+	testutil.AssertViewEqual(t, want, got, nil)
 }
 
 func testJoinSingleKey(t *testing.T) {
@@ -31,9 +51,9 @@ func testJoinSingleKey(t *testing.T) {
 	view := Join(v1, v2, key1, key2)
 
 	want := [][]string{
-		{"1", "go", "100", "1", "1", "dockit", "github.com/midbel/dockit"},
-		{"1", "go", "100", "2", "1", "sweet", "github.com/midbel/sweet-ql"},
-		{"2", "js", "50", "3", "2", "dockit-ui", "github.com/midbel/dockit-ui"},
+		{"1", "go", "100", "midbel", "1", "1", "midbel", "dockit", "github.com/midbel/dockit"},
+		{"1", "go", "100", "midbel", "2", "1", "midbel", "sweet", "github.com/midbel/sweet-ql"},
+		{"2", "js", "50", "midbel", "3", "2", "nobody", "dockit-ui", "github.com/midbel/dockit-ui"},
 	}
 	got := testutil.Collect(view)
 	testutil.AssertSize(t, view, got)
