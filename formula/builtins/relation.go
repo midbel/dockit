e6a3de523143ce25b2012a71ccaeab3473418a7c
Nicolas Brun <nbrun@gld.be>
2026-04-26 01:24:01 +0200

    additional builtins

--- formula/builtins/relation.go
+++ formula/builtins/relation.go
@@ -2,6 +2,7 @@ package builtins
 
 import (
 	"github.com/midbel/dockit/formula/types"
+	"github.com/midbel/dockit/grid"
 	gbs "github.com/midbel/dockit/grid/builtins"
 	"github.com/midbel/dockit/gridx"
 	"github.com/midbel/dockit/layout"
@@ -46,15 +47,83 @@ var groupBuiltin = gbs.Builtin{
 	Name:     "group",
 	Desc:     "",
 	Category: "relation",
-	Params:   []gbs.Param{},
-	Func:     Group,
+	Params: []gbs.Param{
+		gbs.Object("sheet", "", value.TypeAny),
+	},
+	Func: Group,
 }
 
 func Group(args []value.Value) value.Value {
 	return value.ErrValue
 }
 
+var unionBuiltin = gbs.Builtin{
+	Name:     "union",
+	Desc:     "",
+	Category: "relation",
+	Params: []gbs.Param{
+		gbs.Object("sheet1", "", value.TypeAny),
+		gbs.Object("sheet2", "", value.TypeAny),
+	},
+	Func: Union,
+}
+
+func Union(args []value.Value) value.Value {
+	return combineViews(args[0], args[1], gridx.Union)
+}
+
+var intersectBuiltin = gbs.Builtin{
+	Name:     "intersect",
+	Desc:     "",
+	Category: "relation",
+	Params: []gbs.Param{
+		gbs.Object("sheet1", "", value.TypeAny),
+		gbs.Object("sheet2", "", value.TypeAny),
+	},
+	Func: Group,
+}
+
+func Intersect(args []value.Value) value.Value {
+	return combineViews(args[0], args[1], gridx.Intersect)
+}
+
+var exceptBuiltin = gbs.Builtin{
+	Name:     "except",
+	Desc:     "",
+	Category: "relation",
+	Params: []gbs.Param{
+		gbs.Object("sheet1", "", value.TypeAny),
+		gbs.Object("sheet2", "", value.TypeAny),
+	},
+	Func: Group,
+}
+
+func Except(args []value.Value) value.Value {
+	return combineViews(args[0], args[1], gridx.Except)
+}
+
+type combineFunc func(grid.View, grid.View) (grid.View, error)
+
+func combineViews(v1 value.Value, v2 value.Value, fn combineFunc) value.Value {
+	left, ok := v1.(*types.View)
+	if !ok {
+		return value.ErrValue
+	}
+	right, ok := v2.(*types.View)
+	if !ok {
+		return value.ErrValue
+	}
+	v, err := fn(left.View(), right.View())
+	if err != nil {
+		return value.ErrValue
+	}
+	return types.NewViewValue(v)
+}
+
 var relationBuiltins = []gbs.Builtin{
 	joinBuiltin,
 	groupBuiltin,
+	unionBuiltin,
+	intersectBuiltin,
+	exceptBuiltin,
 }
