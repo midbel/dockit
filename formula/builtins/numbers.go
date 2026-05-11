fa704e857bb9274a53ee15194f78da62a8939d93
Nicolas Brun <nbrun@gld.be>
2026-05-11 20:23:26 +0200

    seq builtin for date and number

--- formula/builtins/numbers.go
+++ formula/builtins/numbers.go
@@ -1,7 +1,10 @@
 package builtins
 
 import (
+	"time"
+
 	gbs "github.com/midbel/dockit/grid/builtins"
+	"github.com/midbel/dockit/internal/slx"
 	"github.com/midbel/dockit/value"
 )
 
@@ -18,7 +21,47 @@ var seqBuiltin = gbs.Builtin{
 }
 
 func Seq(args []value.Value) value.Value {
-	return nil
+	if err := value.HasErrors(args...); err != nil {
+		return err
+	}
+	var (
+		step  = asFloat(args[1])
+		count = asFloat(args[2])
+	)
+	switch a := args[0].(type) {
+	case value.Date:
+		return generateDateSeq(time.Time(a), int64(step), int64(count))
+	case value.Float:
+		return generateNumberSeq(float64(a), int64(step), int64(count))
+	default:
+		return value.ErrValue
+	}
+}
+
+func generateDateSeq(start time.Time, step, count int64) value.Value {
+	arr := make([][]value.ScalarValue, count)
+	for i := int64(0); i < count; i++ {
+		var (
+			t = start.Add(time.Duration(step*24) * time.Hour)
+			x value.ScalarValue
+		)
+		x = value.Date(t)
+		arr[i] = slx.One(x)
+	}
+	return value.NewArray(arr)
+}
+
+func generateNumberSeq(start float64, step, count int64) value.Value {
+	arr := make([][]value.ScalarValue, count)
+	for i := int64(0); i < count; i++ {
+		var (
+			t = start + float64(i*step)
+			x value.ScalarValue
+		)
+		x = value.Float(t)
+		arr[i] = slx.One(x)
+	}
+	return value.NewArray(arr)
 }
 
 var rangeBuiltin = gbs.Builtin{
