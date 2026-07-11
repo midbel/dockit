94014f01847d050fe59e35f84e50f4d4d6a60030
Nicolas Brun <nbrun@gld.be>
2026-07-11 21:22:38 +0200

    mutation type

---
+++ grid/mutation.go
@@ -0,0 +1,86 @@
+package grid
+
+import (
+	"github.com/midbel/dockit/layout"
+	"github.com/midbel/dockit/value"
+)
+
+type MutationAxis int8
+
+const (
+	Row MutationAxis = 1 << iota
+	Column
+)
+
+type MutationKind int8
+
+const (
+	Insertion MutationKind = 1 << iota
+	Removal
+)
+
+type Mutation struct {
+	Kind   MutationKind
+	Axis   MutationAxis
+	Count  int64
+	Offset int64
+}
+
+func RemoveRows(offset, count int64) Mutation {
+	return Mutation{
+		Kind:   Removal,
+		Axis:   Row,
+		Count:  count,
+		Offset: offset,
+	}
+}
+
+func RemoveColumns(offset, count int64) Mutation {
+	return Mutation{
+		Kind:   Removal,
+		Axis:   Column,
+		Count:  count,
+		Offset: offset,
+	}
+}
+
+func InsertRows(offset, count int64) Mutation {
+	return Mutation{
+		Kind:   Insertion,
+		Axis:   Row,
+		Count:  count,
+		Offset: offset,
+	}
+}
+
+func InsertColumns(offset, count int64) Mutation {
+	return Mutation{
+		Kind:   Insertion,
+		Axis:   Column,
+		Count:  count,
+		Offset: offset,
+	}
+}
+
+type Rangeable interface {
+	Range(layout.Position, layout.Position) value.Value
+	SetRange(layout.Position, layout.Position, value.Value) error
+}
+
+type WritableRange struct {
+	view  Rangeable
+	start layout.Position
+	end   layout.Position
+}
+
+func NewWritableRange(view Rangeable, start, end layout.Position) *WritableRange {
+	return &WritableRange{
+		view:  view,
+		start: start,
+		end:   end,
+	}
+}
+
+func (w WritableRange) SetRange(data value.Value) error {
+	return w.view.SetRange(w.start, w.end, data)
+}
